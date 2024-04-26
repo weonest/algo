dea1fad4901028f407c80030c5a6b303388922c9
weonest <geonhee33@naver.com>
2024-04-27 00:09:17 +0900

    프로그래머스 단속 카메라 리팩토링

--- src/pgs/greedy/SpeedCamera.java
+++ src/pgs/greedy/SpeedCamera.java
@@ -2,6 +2,7 @@ package pgs.greedy;
 
 import java.util.*;
 
+// 단속 카메라
 public class SpeedCamera {
 
     public int solution(int[][] routes) {
@@ -9,16 +10,12 @@ public class SpeedCamera {
 
         Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
 
-        System.out.println(Arrays.deepToString(routes));
-
         int end = routes[0][1];
         for (int i = 1; i < routes.length; i++) {
             int curStart = routes[i][0];
             int curEnd = routes[i][1];
 
-            if (end >= curStart && end <= curEnd) {
-                continue;
-            } else {
+            if (end < curStart) {
                 answer++;
                 end = curEnd;
             }
