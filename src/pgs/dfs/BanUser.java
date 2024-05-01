b3d0a8d5077a64a71a788d79deecbf341e7e610b
weonest <geonhee33@naver.com>
2024-05-01 15:06:31 +0900

    프로그래머스 불량 사용자 (조합 or 순열)

---
+++ src/pgs/dfs/BanUser.java
@@ -0,0 +1,56 @@
+package pgs.dfs;
+
+import java.util.*;
+
+// 불량 사용자
+public class BanUser {
+
+    String[] userIds;
+    String[] bannedIds;
+
+    HashSet<HashSet<String>> result = new HashSet<>();
+
+    public int solution(String[] user_id, String[] banned_id) {
+        userIds = user_id;
+        bannedIds = banned_id;
+
+        dfs(new HashSet<>(), 0);
+
+        return result.size();
+    }
+
+    void dfs(HashSet<String> set, int depth) {
+        if (depth == bannedIds.length) {
+            result.add(set);
+            return;
+        }
+
+        for (int i = 0; i < userIds.length; i++) {
+            if (set.contains(userIds[i])) {
+                continue;
+            }
+
+            if (check(userIds[i], bannedIds[depth])) {
+                set.add(userIds[i]);
+                dfs(new HashSet<>(set), depth + 1);
+                set.remove(userIds[i]);
+            }
+        }
+    }
+
+    boolean check(String userId, String bannedId) {
+        if (userId.length() != bannedId.length()) {
+            return false;
+        }
+
+        boolean match = true;
+        for (int i = 0; i < userId.length(); i++) {
+            if (bannedId.charAt(i) != '*' && userId.charAt(i) != bannedId.charAt(i)) {
+                match = false;
+                break;
+            }
+        }
+
+        return match;
+    }
+}
